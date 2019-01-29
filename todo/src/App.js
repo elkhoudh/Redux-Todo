a728097cb8cd3d53a34b91c9076288132763dcff
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-01-29 18:49:44 -0500

    Changed some styling

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import NavBar from "./components/NavBar";
 import Modal from "./components/Modal";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import cyan from "@material-ui/core/colors/cyan";
+const theme = createMuiTheme({
+  palette: {
+    primary: cyan,
+    type: "dark"
+  }
+});
 class App extends Component {
   render() {
     return (
-      <div>
+      <MuiThemeProvider theme={theme}>
         <NavBar />
         <Modal />
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
